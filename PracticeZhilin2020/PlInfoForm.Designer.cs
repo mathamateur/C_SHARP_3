ec5d4442ee708449ea595ef79a72bce8389bede7
x-sanchez <sanya0112@mail.ru>
2020-05-11 00:09:09 +0300

    PlInfo

---
+++ PracticeZhilin2020/PlInfoForm.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace PracticeZhilin2020
+{
+    partial class PlInfoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlInfoForm));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.redCards = new System.Windows.Forms.Label();
+            this.yellowCards = new System.Windows.Forms.Label();
+            this.assists = new System.Windows.Forms.Label();
+            this.goals = new System.Windows.Forms.Label();
+            this.workFoot = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dateOfBirth = new System.Windows.Forms.Label();
+            this.lastName = new System.Windows.Forms.Label();
+            this.nationality = new System.Windows.Forms.Label();
+            this.position = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BackColor = System.Drawing.Color.Transparent;
+            this.splitContainer1.Location = new System.Drawing.Point(48, 44);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.redCards);
+            this.splitContainer1.Panel2.Controls.Add(this.yellowCards);
+            this.splitContainer1.Panel2.Controls.Add(this.assists);
+            this.splitContainer1.Panel2.Controls.Add(this.goals);
+            this.splitContainer1.Panel2.Controls.Add(this.workFoot);
+            this.splitContainer1.Panel2.Controls.Add(this.label15);
+            this.splitContainer1.Panel2.Controls.Add(this.name);
+            this.splitContainer1.Panel2.Controls.Add(this.label13);
+            this.splitContainer1.Panel2.Controls.Add(this.label12);
+            this.splitContainer1.Panel2.Controls.Add(this.label11);
+            this.splitContainer1.Panel2.Controls.Add(this.label10);
+            this.splitContainer1.Panel2.Controls.Add(this.dateOfBirth);
+            this.splitContainer1.Panel2.Controls.Add(this.lastName);
+            this.splitContainer1.Panel2.Controls.Add(this.nationality);
+            this.splitContainer1.Panel2.Controls.Add(this.position);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(702, 362);
+            this.splitContainer1.SplitterDistance = 351;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 356);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // redCards
+            // 
+            this.redCards.AutoSize = true;
+            this.redCards.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.redCards.Location = new System.Drawing.Point(171, 324);
+            this.redCards.Name = "redCards";
+            this.redCards.Size = new System.Drawing.Size(93, 25);
+            this.redCards.TabIndex = 19;
+            this.redCards.Text = "redCards";
+            // 
+            // yellowCards
+            // 
+            this.yellowCards.AutoSize = true;
+            this.yellowCards.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.yellowCards.Location = new System.Drawing.Point(171, 292);
+            this.yellowCards.Name = "yellowCards";
+            this.yellowCards.Size = new System.Drawing.Size(119, 25);
+            this.yellowCards.TabIndex = 18;
+            this.yellowCards.Text = "yellowCards";
+            // 
+            // assists
+            // 
+            this.assists.AutoSize = true;
+            this.assists.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.assists.Location = new System.Drawing.Point(172, 256);
+            this.assists.Name = "assists";
+            this.assists.Size = new System.Drawing.Size(72, 25);
+            this.assists.TabIndex = 17;
+            this.assists.Text = "assists";
+            // 
+            // goals
+            // 
+            this.goals.AutoSize = true;
+            this.goals.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.goals.Location = new System.Drawing.Point(172, 222);
+            this.goals.Name = "goals";
+            this.goals.Size = new System.Drawing.Size(59, 25);
+            this.goals.TabIndex = 16;
+            this.goals.Text = "goals";
+            // 
+            // workFoot
+            // 
+            this.workFoot.AutoSize = true;
+            this.workFoot.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.workFoot.Location = new System.Drawing.Point(171, 161);
+            this.workFoot.Name = "workFoot";
+            this.workFoot.Size = new System.Drawing.Size(92, 25);
+            this.workFoot.TabIndex = 15;
+            this.workFoot.Text = "workFoot";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label15.Location = new System.Drawing.Point(18, 263);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(143, 18);
+            this.label15.TabIndex = 14;
+            this.label15.Text = "Голевые передачи:";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.name.Location = new System.Drawing.Point(172, 23);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(61, 25);
+            this.name.TabIndex = 13;
+            this.name.Text = "name";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label13.Location = new System.Drawing.Point(18, 229);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(50, 18);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Голы:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label12.Location = new System.Drawing.Point(18, 201);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(72, 18);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Позиция:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label11.Location = new System.Drawing.Point(18, 128);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(129, 18);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Национальность:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(18, 64);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 18);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Фамилия:";
+            // 
+            // dateOfBirth
+            // 
+            this.dateOfBirth.AutoSize = true;
+            this.dateOfBirth.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateOfBirth.Location = new System.Drawing.Point(172, 87);
+            this.dateOfBirth.Name = "dateOfBirth";
+            this.dateOfBirth.Size = new System.Drawing.Size(110, 25);
+            this.dateOfBirth.TabIndex = 8;
+            this.dateOfBirth.Text = "dateOfBirth";
+            // 
+            // lastName
+            // 
+            this.lastName.AutoSize = true;
+            this.lastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lastName.Location = new System.Drawing.Point(172, 57);
+            this.lastName.Name = "lastName";
+            this.lastName.Size = new System.Drawing.Size(94, 25);
+            this.lastName.TabIndex = 7;
+            this.lastName.Text = "lastName";
+            // 
+            // nationality
+            // 
+            this.nationality.AutoSize = true;
+            this.nationality.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.nationality.Location = new System.Drawing.Point(172, 121);
+            this.nationality.Name = "nationality";
+            this.nationality.Size = new System.Drawing.Size(99, 25);
+            this.nationality.TabIndex = 6;
+            this.nationality.Text = "nationality";
+            // 
+            // position
+            // 
+            this.position.AutoSize = true;
+            this.position.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.position.Location = new System.Drawing.Point(172, 194);
+            this.position.Name = "position";
+            this.position.Size = new System.Drawing.Size(79, 25);
+            this.position.TabIndex = 5;
+            this.position.Text = "position";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(18, 331);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 18);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Красные карточки:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(18, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(137, 18);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Жёлтые карточки:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(18, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Рабочая нога:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(18, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Дата рождения:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(18, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Имя:";
+            // 
+            // PlInfoForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PlInfoForm";
+            this.Text = "PlInfoForm";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label redCards;
+        private System.Windows.Forms.Label yellowCards;
+        private System.Windows.Forms.Label assists;
+        private System.Windows.Forms.Label goals;
+        private System.Windows.Forms.Label workFoot;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label dateOfBirth;
+        private System.Windows.Forms.Label lastName;
+        private System.Windows.Forms.Label nationality;
+        private System.Windows.Forms.Label position;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PracticeZhilin2020/PlInfoForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace PracticeZhilin2020
+{
+    public partial class PlInfoForm : Form
+    {
+        int id_player;
+        public PlInfoForm(int id_player)
+        {
+            InitializeComponent();
+            this.id_player = id_player;
+            ObjectInfoSystem.Player player = new ObjectInfoSystem.Player();
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM players WHERE Id = @i_p", db.getConnection());
+            command.Parameters.Add("@i_p", MySqlDbType.Int32).Value = id_player;
+            db.openConnection();
+            MySqlDataReader reader = command.ExecuteReader();
+            if (reader.Read())
+            {
+                name.Text = reader["first_name"].ToString();
+                lastName.Text = reader["last_name"].ToString();
+                dateOfBirth.Text = reader["date_of_birth"].ToString().Substring(0,10);
+                nationality.Text = reader["nationality"].ToString();
+                workFoot.Text = reader["work_foot"].ToString();
+                position.Text = reader["positions"].ToString();
+                goals.Text = reader["goals"].ToString();
+                assists.Text = reader["assists"].ToString();
+                yellowCards.Text = reader["yellow_cards"].ToString();
+                redCards.Text = reader["red_cards"].ToString();
+            }
+            reader.Close();
+        }
+    }
+}
